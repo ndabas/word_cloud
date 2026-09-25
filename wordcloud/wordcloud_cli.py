17c1c0a21822bb3f76bfa264515219366d38dcc3
Nikhil Dabas <nikhil@nikhildabas.com>
2026-09-25 10:32:33 +0100

    Add explicit prog= to ArgumentParser
    Python 3.14 derives prog from sys.orig_argv, which is the full python.exe command line for Windows console-script launchers.

--- wordcloud/wordcloud_cli.py
+++ wordcloud/wordcloud_cli.py
@@ -97,7 +97,7 @@ def main(args, text, imagefile):
 
 def make_parser():
     description = 'A simple command line interface for wordcloud module.'
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(prog='wordcloud_cli', description=description)
     parser.add_argument(
         '--text', metavar='file', type=FileType(), default='-',
         help='specify file of words to build the word cloud (default: stdin)')
